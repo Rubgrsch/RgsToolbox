f6f30cd0e6b64acb5a85e75c236d2db09c702ce1
Rubgrsch <rubgrsch@gmail.com>
2018-08-10 11:00:13 +0800

    Fix DB not saving

--- config.lua
+++ config.lua
@@ -1,27 +1,23 @@
 local _, rgsaddon = ...
 local C, R = unpack(rgsaddon)
-local E, _, _, _, G = unpack(ElvUI)
+local E, _, _, P = unpack(ElvUI)
 
 local pairs = pairs
 
-local defaults = {
+P.RgsToolbox = {
 	stats = false,
 	stealth = false,
 	namehide = false,
 }
 
 R:AddInitFunc(function()
-	C.db = G.RgsToolbox or {}
-	-- fallback to defaults
-	for k,v in pairs(defaults) do if C.db[k] == nil then C.db[k] = v end end
-	-- Start of DB Conversion
-	-- End of DB conversion
-	for k in pairs(C.db) do if defaults[k] == nil then C.db[k] = nil end end -- remove old keys
+	if not E.db.RgsToolbox then E.db.RgsToolbox = {} end
+	C.db = E.db.RgsToolbox
 end)
 
 function R:InsertOptions()
 	E.Options.args.RgsToolbox = {
-		order = 100,
+		order = 1000,
 		type = "group",
 		name = "RgsToolbox",
 		get = function(info) return C.db[info[#info]] end,
