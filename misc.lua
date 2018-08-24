5153aad78f735dd2c677e5e8fc2841f290552838
Rubgrsch <rubgrsch@gmail.com>
2018-08-24 14:17:38 +0800

    Make these fixes only available for zhCN

--- misc.lua
+++ misc.lua
@@ -5,14 +5,16 @@ hooksecurefunc(StaticPopupDialogs["DELETE_GOOD_ITEM"],"OnShow",function(s) s.edi
 BossBanner:UnregisterEvent("ENCOUNTER_LOOT_RECEIVED")
 BossBanner:UnregisterEvent("BOSS_KILL")
 
-StaticPopupDialogs["LFG_LIST_ENTRY_EXPIRED_TOO_MANY_PLAYERS"] = {
-	text = "针对此项活动，你的队伍人数已满，将被移出列表。",
-	button1 = OKAY,
-	timeout = 0,
-	whileDead = 1,
-}
+if GetLocale() == "zhCN" then
+	StaticPopupDialogs["LFG_LIST_ENTRY_EXPIRED_TOO_MANY_PLAYERS"] = {
+		text = "针对此项活动，你的队伍人数已满，将被移出列表。",
+		button1 = OKAY,
+		timeout = 0,
+		whileDead = 1,
+	}
 
-if not GuildControlUIRankSettingsFrameRosterLabel then
-	GuildControlUIRankSettingsFrameRosterLabel = CreateFrame("Frame")
-	GuildControlUIRankSettingsFrameRosterLabel:Hide()
+	if not GuildControlUIRankSettingsFrameRosterLabel then
+		GuildControlUIRankSettingsFrameRosterLabel = CreateFrame("Frame")
+		GuildControlUIRankSettingsFrameRosterLabel:Hide()
+	end
 end
