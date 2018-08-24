c22bc5fb18ded228d61705a896d2b4b737a91418
Rubgrsch <rubgrsch@gmail.com>
2018-08-24 13:56:45 +0800

    Add LFG lua error fix

--- misc.lua
+++ misc.lua
@@ -5,6 +5,12 @@ hooksecurefunc(StaticPopupDialogs["DELETE_GOOD_ITEM"],"OnShow",function(s) s.edi
 BossBanner:UnregisterEvent("ENCOUNTER_LOOT_RECEIVED")
 BossBanner:UnregisterEvent("BOSS_KILL")
 
+StaticPopupDialogs["LFG_LIST_ENTRY_EXPIRED_TOO_MANY_PLAYERS"] = {
+	text = "针对此项活动，你的队伍人数已满，将被移出列表。",
+	button1 = OKAY,
+	timeout = 0,
+	whileDead = 1,
+}
 
 if not GuildControlUIRankSettingsFrameRosterLabel then
 	GuildControlUIRankSettingsFrameRosterLabel = CreateFrame("Frame")
