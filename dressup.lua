78cafc5de5a1501accc0512b4cb0b74edb21c6da
Rubgrsch <rubgrsch@gmail.com>
2018-08-27 11:26:11 +0800

    Undress first

---
+++ dressup.lua
@@ -0,0 +1,22 @@
+
+function DressUpVisual(...)
+	if ( SideDressUpFrame.parentFrame and SideDressUpFrame.parentFrame:IsShown() ) then
+		if ( not SideDressUpFrame:IsShown() or SideDressUpFrame.mode ~= "player" ) then
+			SideDressUpFrame.mode = "player";
+			SideDressUpFrame.ResetButton:Show();
+
+			local race, fileName = UnitRace("player");
+			SetDressUpBackground(SideDressUpFrame, fileName);
+
+			ShowUIPanel(SideDressUpFrame);
+			SideDressUpModel:SetUnit("player");
+		end
+		SideDressUpModel:Undress()
+		SideDressUpModel:TryOn(...);
+	else
+		DressUpFrame_Show();
+		DressUpModel:Undress()
+		DressUpModel:TryOn(...);
+	end
+	return true;
+end
