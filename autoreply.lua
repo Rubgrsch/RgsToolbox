80c5db9e46d540906c125afd56f36fdaf20fe263
Rubgrsch <rubgrsch@gmail.com>
2020-02-02 22:58:07 +0800

    More fix for last commit

--- autoreply.lua
+++ autoreply.lua
@@ -5,9 +5,9 @@ local function replyString()
 	local _, _, steps = C_Scenario_GetStepInfo()
 	local bosses, totalbosses, progress = 0, 0
 	for i=1, steps do
-		local _, _, _, quantity, totalQuantity, _, _, _, _, _, _, _, isWeightedProgress = C_Scenario_GetCriteriaInfo(i)
+		local _, _, completed, quantity, totalQuantity, _, _, _, _, _, _, _, isWeightedProgress = C_Scenario_GetCriteriaInfo(i)
 		if isWeightedProgress then
-			progress = quantity/totalQuantity
+			progress = completed and 100 or quantity
 		else
 			bosses = bosses + quantity
 			totalbosses = totalbosses + totalQuantity
@@ -15,7 +15,7 @@ local function replyString()
 	end
 	local dungeonLevel = C_ChallengeMode_GetActiveKeystoneInfo()
 	local dungeonName = C_Scenario_GetInfo()
-	return format("正在进行史诗钥石 +%d %s | 首领%d/%d | 进度%.0f%%", dungeonLevel, dungeonName, bosses, totalbosses, 100*progress)
+	return format("正在进行史诗钥石 +%d %s | 首领%d/%d | 进度%.0f%%", dungeonLevel, dungeonName, bosses, totalbosses, progress)
 end
 
 local frame = CreateFrame("Frame")
@@ -26,12 +26,12 @@ local function SentMessage(target)
 	sentPlayers[target] = true
 	if not wipeTimer then
 		wipeTimer = true
-		C_Timer_After(30, WipePlayers)
+		C_Timer_After(60, WipePlayers)
 	end
 end
 
 function frame:CHAT_MSG_WHISPER(_, _, sender, _, _, _, _, _, _, _, _, _, guid)
-	if C_ChallengeMode_IsChallengeModeActive() and (C_BattleNet_GetGameAccountInfoByGUID(guid) or IsGuildMember(guid) or C_FriendList_IsFriend(guid)) and not sentPlayers[sender] then
+	if C_ChallengeMode_IsChallengeModeActive() and (C_BattleNet_GetGameAccountInfoByGUID(guid) or IsGuildMember(guid) or C_FriendList_IsFriend(guid)) and not (UnitInRaid(sender) or UnitInParty(sender)) and not sentPlayers[sender] then
 		SentMessage(sender)
 		SendChatMessage(replyString(), "WHISPER", nil, sender)
 	end
@@ -39,6 +39,20 @@ end
 
 function frame:CHAT_MSG_BN_WHISPER(_, _, _, _, _, _, _, _, _, _, _, _, _, bnSenderID)
 	if C_ChallengeMode_IsChallengeModeActive() and not sentPlayers[bnSenderID] then
+		local index = BNGetFriendIndex(bnSenderID)
+		local gameAccs = C_BattleNet.GetFriendNumGameAccounts(index)
+		for i=1, gameAccs do
+			local gameAccountInfo = C_BattleNet.GetFriendGameAccountInfo(index, i)
+			if gameAccountInfo.clientProgram == "WoW" then
+				local player = gameAccountInfo.characterName
+				if gameAccountInfo.realmName ~= GetRealmName() then
+					player = player .. "-" .. gameAccountInfo.realmName
+				end
+				if UnitInRaid(player) or UnitInParty(player) then
+					return
+				end
+			end
+		end
 		SentMessage(bnSenderID)
 		BNSendWhisper(bnSenderID, replyString())
 	end
